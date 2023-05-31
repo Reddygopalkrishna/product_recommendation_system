0e93292f04fecaa2811ad3010472b3ad49f7f966
Reddy Gopala Krishna Pakala <122770274+Reddygopalkrishna@users.noreply.github.com>
2023-05-31 13:47:18 +0530

    Update product_recommendation_system.py

--- product_recommendation_system.py
+++ product_recommendation_system.py
@@ -1,1 +1,38 @@
+import numpy as np
+
+users = ["user1","user2","user3"]
+products = ["laptop","mouse","monitor","pen drive","hard disk"]
+
+uids = {} #dict
+for i in range(len(users)):
+  uids[users[i]]=i
+
+pids = {} # dict
+
+for i in range(len(products)):
+  pids[products[i]]=i
+
+print(uids, pids)
+
+prd_fq = np.zeros((len(users),len(products)) )
+print(prd_fq)
+
+def reclist(uid):
+  uid = uids[uid]
+  upids = prd_fq[uid, :]
+  plist = np.argsort(upids)[::-1][:len(upids)]
+  plist = [products[p] for p in plist]
+  print(plist)
+
+def viewproduct(uid, pid):
+  uid = uids[uid]
+  pid = pids[pid]
+  prd_fq[uid, pid] = prd_fq[uid, pid] + 1
+  print(prd_fq)
+
+
+uname = input("Enter user name:")
+print(reclist(uname))
+pname = input("Select one product:")
+viewproduct(uname, pname)
 
