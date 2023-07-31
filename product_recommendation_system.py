c9081aa5697cc46c7f14e621255c86db79887625
Reddy Gopala Krishna Pakala <122770274+Reddygopalkrishna@users.noreply.github.com>
2023-07-31 20:34:50 +0530

    Update product_recommendation_system.py

--- product_recommendation_system.py
+++ product_recommendation_system.py
@@ -1,43 +1,39 @@
-import numpy as np       # numpy module
+import numpy as np  # Import the numpy module for numerical operations
 
-users = ["user1","user2","user3"]  # users
+users = ["user1", "user2", "user3"]  # List of user names
 
-products = ["laptop","mouse","monitor","pen drive","hard disk"] # products
+products = ["laptop", "mouse", "monitor", "pen drive", "hard disk"]  # List of product names
 
-uids = {} #dict
 
+uids = {}  # Dictionary to store user IDs
 for i in range(len(users)):
-  uids[users[i]]=i
+    uids[users[i]] = i  
 
-pids = {} # dict
-
+pids = {}  # Dictionary to store product IDs
 for i in range(len(products)):
-  pids[products[i]]=i
+    pids[products[i]] = i  
 
-print(uids, pids)
+print(uids, pids)  
 
-prd_fq = np.zeros((len(users),len(products)) )   # Product frequency 
-print(prd_fq)
+prd_fq = np.zeros((len(users), len(products)))  # Create a 2D array to store the product frequency matrix
+print(prd_fq)  # Print the product frequency matrix (initialized with zeros)
 
 
 def reclist(uid):
-  uid = uids[uid]
-  upids = prd_fq[uid, :]
-  plist = np.argsort(upids)[::-1][:len(upids)]
-  plist = [products[p] for p in plist]
-  print(plist)
-  
+    uid = uids[uid]  
+    upids = prd_fq[uid, :]  # Get the product frequency for the given user from the product frequency matrix
+    plist = np.argsort(upids)[::-1][:len(upids)]  # Sort the product frequency in descending order and get the indices
+    plist = [products[p] for p in plist]  # Convert the sorted indices back to product names
+    print(plist) 
+
 
 def viewproduct(uid, pid):
-  uid = uids[uid]
-  pid = pids[pid]
-  prd_fq[uid, pid] = prd_fq[uid, pid] + 1
-  print(prd_fq)
+    uid = uids[uid]  
+    pid = pids[pid] 
+    prd_fq[uid, pid] = prd_fq[uid, pid] + 1  # Increment the product frequency for the user and product combination
+    print(prd_fq)  
 
+uname = input("Enter user name:")  print(reclist(uname))  # Call the reclist function to get the recommended products for the given user
 
-uname = input("Enter user name:")
-print(reclist(uname))
-
-pname = input("Select one product:")
-viewproduct(uname, pname)
-
+pname = input("Select one product:")  
+viewproduct(uname, pname)  
